c4ad6cc7369443fde73b056119cfc8ee162175a9
PV <7860379+pwl482@users.noreply.github.com>
2021-03-02 12:02:01 +0100

    new plots added

--- SDBS_App.py
+++ SDBS_App.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 filename = r'D:\GeospacialDBs_Data\countries.geo.json'
 with open(filename, 'r') as file:
@@ -36,6 +37,16 @@ colors = {
 
 slider_values = df_apple["timestamp"].unique()
 slider_labels = [(i, slider_values[i]) for i in range(len(slider_values)) if i % 20 == 0]
+
+countries_list = df_apple['region'].unique()
+dict_list = []
+for i in countries_list:
+    dict_list.append({'label': i, 'value': i})
+
+types_list = df_apple['transportation_type'].unique()
+dict_list_type = []
+for i in types_list:
+    dict_list_type.append({'label': i, 'value': i})
 
 
 @app.callback(Output('world_graph', 'figure'),
@@ -79,27 +90,88 @@ def update_cases(selected_slider_value):
     return fig
 
 
+@app.callback(Output('countries_graph', 'figure'),
+              [Input('dropdown_countries', 'value'), Input('dropdown_types', 'value')])
+def update_countries(selected_dropdown_values, selected_dropdown_values2):
+    trace = []
+    if selected_dropdown_values is not None or  selected_dropdown_values2 is not None:
+        for country in selected_dropdown_values:
+            df_sub_countries = df_apple_geo[df_apple_geo['region'] == country]
+            for type in selected_dropdown_values2:
+                trace.append(go.Scatter(x=df_sub_countries[df_sub_countries['transportation_type'] == type]['timestamp'],
+                                        y=df_sub_countries[df_sub_countries['transportation_type'] == type]['percentage'],
+                                        mode='lines',
+                                        opacity=0.7,
+                                        name=country + ":" + type,
+                                        textposition='bottom center'))
+    traces = [trace]
+    data = [val for sublist in traces for val in sublist]
+    fig = {'data': data,
+              'layout': go.Layout(
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+                  margin={'b': 15},
+                  hovermode='x',
+                  autosize=True
+              )}
+    return fig
+
+
+@app.callback(Output('tabs-example-content', 'children'),
+              Input('tabs-example', 'value'))
+def render_tab_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.Label('Select Graph Type', style={'textAlign': 'center', 'color': colors['text']}),
+            dcc.Dropdown(
+                options=dict_list_type,
+                value=types_list[0], style={
+                    'textAlign': 'center',
+                    'color': colors['text']},
+                id='dropdown'
+            ),
+            dcc.Graph(id='world_graph')
+            ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.Div(className='country_selector', children=[
+                    html.Label('Select Countries:', style={'textAlign': 'center', 'color': colors['text']}),
+                    dcc.Dropdown(
+                        options=dict_list,
+                        multi=True, style={
+                            'textAlign': 'center',
+                            'color': colors['text']},
+                        id='dropdown_countries'
+                    )], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+            html.Div(className='class_selector', children=[
+                html.Label('Select Type:', style={'textAlign': 'center', 'color': colors['text']}),
+                dcc.Dropdown(
+                    options=dict_list_type,
+                    multi=True, style={
+                        'textAlign': 'center',
+                        'color': colors['text']},
+                    id='dropdown_types'
+                )], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+            dcc.Graph(id='countries_graph')
+        ])
+
+
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(children='Covid-19 Cases and Traffic data app', style={
-            'textAlign': 'center',
-            'color': colors['text']}),
     html.Div(className='four columns div-user-controls', children=[
+        html.H1(children='Covid-19 Cases and Traffic data app', style={
+                'textAlign': 'center',
+                'color': "white",
+                'background': '#1abc9c',
+                'font-size': '30px',
+                'padding': '20px'}),
         html.Label('New Cases per Day', style={'textAlign': 'center', 'color': colors['text']}),
         dcc.Graph(id='cases_graph')], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),
     html.Div(className='eight columns div-for-charts bg-grey', children=[
-        html.Label('Select Graph Type', style={'textAlign': 'center', 'color': colors['text']}),
-        dcc.Dropdown(
-            options=[
-                {'label': 'Driving', 'value': 'driving'},
-                {'label': 'Walking', 'value': 'walking'},
-                {'label': 'Transit', 'value': 'transit'}
-            ],
-            value='driving', style={
-                'textAlign': 'center',
-                'color': colors['text']},
-            id='dropdown'
-        ),
-        dcc.Graph(id='world_graph')], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+        dcc.Tabs(id='tabs-example', value='tab-1', children=[
+            dcc.Tab(label='Change by Day (% change)', value='tab-1'),
+            dcc.Tab(label='Change over whole Time (% change)', value='tab-2'),
+        ]),
+        html.Div(id='tabs-example-content')],
+             style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),
     html.Label('Date Slider', style={'textAlign': 'center', 'color': colors['text']}),
     dcc.Slider(
         min=0,
